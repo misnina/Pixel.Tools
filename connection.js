0f82699e7a386ad9e71caeb9b76c9edde6fd4ac9
Nina <ninarachaebuie@gmail.com>
2019-10-23 01:08:36 -0500

    changed peerJS to use default host

--- connection.js
+++ connection.js
@@ -1,9 +1,9 @@
 var peer;
 
-function connectStart(){
+function connectStart() {
 
 	//custom server
-    //peer = new Peer( $('#connect_alias').val(), {host: '54.86.240.189', port: 9000, path: '/'});
+	//peer = new Peer( $('#connect_alias').val(), {host: '54.86.240.189', port: 9000, path: '/'});
 
 	// Connect to PeerJS, have server assign an ID instead of providing one
 	// Showing off some of the configs available with PeerJS :).
@@ -24,36 +24,34 @@ function connectStart(){
 		'ya0fxycbbyfirudi',
 		'pss9zv24dl8r529'
 	];
-	
-	peer = new Peer( $('#connect_alias').val(), {
-	  // Set API key for cloud server (you don't need this if you're running your
-	  // own.
-	  key: room[$('#connect_room').val()],
-		
-	  // Set highest debug level (log everything!).
-	  debug: 3,
 
-	  config: {'iceServers': [
-		{ url: 'stun:stun.l.google.com:19302' },
-		{url:'stun:stun1.l.google.com:19302'},
-		{url:'stun:stun2.l.google.com:19302'},
-		{url:'stun:stun3.l.google.com:19302'},
-		{url:'stun:stun4.l.google.com:19302'}
-	  ]}, // Sample servers, please use appropriate ones 
-	  
-	  // Set a logging function:
-	  logFunction: function() {
-		var copy = Array.prototype.slice.call(arguments).join(' ');
-		$('#msgbox').append('<div style="direction:ltr;">'+copy + '</div>');
-	  }
+	peer = new Peer($('#connect_alias').val(), {
+		// Set highest debug level (log everything!).
+		debug: 3,
+
+		config: {
+			'iceServers': [
+				{ url: 'stun:stun.l.google.com:19302' },
+				{ url: 'stun:stun1.l.google.com:19302' },
+				{ url: 'stun:stun2.l.google.com:19302' },
+				{ url: 'stun:stun3.l.google.com:19302' },
+				{ url: 'stun:stun4.l.google.com:19302' }
+			]
+		}, // Sample servers, please use appropriate ones 
+
+		// Set a logging function:
+		logFunction: function () {
+			var copy = Array.prototype.slice.call(arguments).join(' ');
+			$('#msgbox').append('<div style="direction:ltr;">' + copy + '</div>');
+		}
 	});
 
 	// Show this peer's ID.
-	peer.on('open', function(id){
-	  $('#pid').text(id);
-	  $('#proom').text(' : '+$('#connect_room option:selected').text()+' studio');
-	  $('#connect_box').hide();
-	  $('#wacom_detect').hide();
+	peer.on('open', function (id) {
+		$('#pid').text(id);
+		$('#proom').text(' : ' + $('#connect_room option:selected').text() + ' studio');
+		$('#connect_box').hide();
+		$('#wacom_detect').hide();
 	});
 
 	// Await connections from others
